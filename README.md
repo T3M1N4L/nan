89176fe91d802a5a77d3978d353890dce460a062
T3RM1N4L <108430379+T3M1N4L@users.noreply.github.com>
2026-02-09 23:56:42 +0000

    init

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# nan
+# NaN - Not-A-Note
 very minimal notes app
+# features
+ctrl+[1-0] for different save slots
+offline viewing through service workers
+saving to local cache
+auto list continuation
---
+++ public/sw.js
@@ -0,0 +1,55 @@
+const CACHE_NAME = 'simple-notes-v2';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/favicon.svg'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+
+        const responseToCache = response.clone();
+
+        caches.open(CACHE_NAME)
+          .then(cache => {
+            cache.put(event.request, responseToCache);
+          });
+
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
